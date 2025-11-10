d89020f138c22741a238a42f74020e174c60a57b
servorx <191423123+servorx@users.noreply.github.com>
2025-11-10 10:11:48 -0500

    feat: :pencil2: rename rol
    se renombraron todos los archivos que se llamaban "rol" a "role"

---
+++ backend/schemas/auth/role.schema.js
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const RoleBaseSchema = z.object({
+  role_name: z.string().min(3).max(20),
+  description: z.string().max(255)
+})
+
+export const RoleCreateSchema = RoleBaseSchema
+export const RoleUpdateSchema = RoleBaseSchema.partial()
+export const RoleResponseSchema = RoleBaseSchema.extend({
+  id: z.number().int(),
+  created_at: z.string(),
+  updated_at: z.string()
+})
--- backend/schemas/auth/user.schema.js
+++ backend/schemas/auth/user.schema.js
@@ -1,58 +1,34 @@
 import { z } from 'zod'
 
-export const registerUserSchema = z.object({
-  username: z
-    .string()
-    .min(3, 'El nombre de usuario debe tener al menos 3 caracteres')
-    .max(50),
-  email: z.email('Email inválido'),
-  password: z
-    .string()
-    .min(8, 'La contraseña debe tener al menos 8 caracteres')
-    .max(255),
-  profile_picture_url: z.string().url().optional(),
-  bio: z.string().max(500).optional(),
-  keyboard: z.string().max(50).optional(),
-  github_username: z.string().optional(),
-  twitter_username: z.string().optional(),
-  website_url: z.url().optional(),
-  profile_score: z.number().default(0),
-  tests_started: z.number().default(0),
-  tests_completed: z.number().default(0),
-  time_typing: z.string().default('00:00:00'),
-  is_active: z.boolean().default(true),
-  preferred_language: z.string().default('en')
+export const UserBaseSchema = z.object({
+  username: z.string().min(3).max(50),
+  password: z.string().min(8).max(255),
+  email: z.email().max(150),
+  profile_picture_url: z.url().nullable().optional(),
+  bio: z.string().nullable().optional(),
+  keyboard: z.string().max(50).nullable().optional(),
+  github_username: z.string().max(50).nullable().optional(),
+  twitter_username: z.string().max(50).nullable().optional(),
+  website_url: z.string().url().nullable().optional(),
+  profile_score: z.number().int().default(0),
+  tests_started: z.number().int().default(0),
+  tests_completed: z.number().int().default(0),
+  time_typing: z.iso.time().nullable().optional(),
+  preferred_language: z.string().length(2).default('en'),
+  is_active: z.boolean().default(true)
 })
 
-export const updateProfileSchema = z.object({
-  username: z
-    .string()
-    .min(3, 'El nombre de usuario debe tener al menos 3 caracteres')
-    .max(50),
-  email: z.email('Email inválido'),
-  password: z
-    .string()
-    .min(8, 'La contraseña debe tener al menos 8 caracteres')
-    .max(255),
-  profile_picture_url: z.string().url().optional(),
-  bio: z.string().max(500).optional(),
-  keyboard: z.string().max(50).optional(),
-  github_username: z.string().optional(),
-  twitter_username: z.string().optional(),
-  website_url: z.url().optional(),
-  profile_score: z.number().default(0),
-  tests_started: z.number().default(0),
-  tests_completed: z.number().default(0),
-  time_typing: z.string().default('00:00:00'),
-  is_active: z.boolean().default(true),
-  preferred_language: z.string().default('en')
+// Crear usuario
+export const UserCreateSchema = UserBaseSchema.pick({
+  username: true,
+  password: true,
+  email: true
 })
 
-export const userResponseSchema = z.object({
-  id: z.number(),
-  username: z.string()
-})
+export const UserUpdateSchema = UserBaseSchema.partial()
 
-export const deleteUserSchema = z.object({
-  id: z.number()
+export const UserResponseSchema = UserBaseSchema.extend({
+  id: z.number().int(),
+  created_at: z.string(),
+  updated_at: z.string()
 })
